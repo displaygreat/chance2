8687681e2e24d5e6ac6868f70b5717cec5c3170d
displaygreat <displaygreat@gmail.com>
2021-08-01 22:54:48 +0300

    disable scroll on opening burger-menu

--- js/main.js
+++ js/main.js
@@ -16,10 +16,35 @@ const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".navbar-panel");
 const closeBtn = document.querySelector(".close-btn");
 
+const disableScroll = () => {
+  const scrollWidth = window.innerWidth - document.body.offsetWidth;
+
+  document.body.dataset.scrollY = window.scrollY;
+
+  document.body.style.cssText = `
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: -${window.scrollY};
+    left: 0;
+    padding-right: ${scrollWidth};
+    overflow: hidden;
+  `;
+};
+
+const enableScroll = () => {
+  document.body.style.cssText = "";
+  window.scroll({
+    top: document.body.dataset.scrollY,
+  });
+};
+
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("is-open");
+  disableScroll();
 });
 
 closeBtn.addEventListener("click", () => {
   menu.classList.toggle("is-open");
+  enableScroll();
 });
